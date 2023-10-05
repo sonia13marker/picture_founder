8092718a203c4114e7833926fc1d5434ce99f1f3
SoNya Marker <soniamarker13@gmail.com>
2023-10-05 23:40:11 +0300

    initial styles and scss

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import './style.scss';
+
+export default function Header () {
+    return (
+        <>
+        </>
+    )
+}
---
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,10 @@
+import './style.scss';
+
+export default function MainPage () {
+    return (
+        <>
+        <h1>hello its ubuntu</h1>
+        <h2>hello its NO ubuntu</h2>
+        </>
+    )
+}
