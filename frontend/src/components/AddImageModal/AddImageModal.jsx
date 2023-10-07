526bdfcfd93d035fba4db4fdce3b1f1bdcad5354
SoNya Marker <soniamarker13@gmail.com>
2023-10-07 23:09:52 +0300

    logic of adding images

--- frontend/src/components/AddImageModal/AddImageModal.jsx
+++ frontend/src/components/AddImageModal/AddImageModal.jsx
@@ -1,7 +1,24 @@
 import './AddImageModal.scss';
 import add_img from '../../images/add_img.svg';
+import { useState } from 'react';
 
 export default function AddImageModal ({active,setActive}) {
+
+    /*for button "add image" */
+    const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setSelectedImage(fileReader.result);
+    };
+
+    if (file) {
+      fileReader.readAsDataURL(file);
+    }
+  };
     return (
         <div className={active ? "modal active" : "modal"}>
             <div className='modal__content' onClick={(e) => e.stopPropagation()}>
@@ -17,7 +34,15 @@ export default function AddImageModal ({active,setActive}) {
                     {/*блок с добавлением картинки drag&drop */}
                     <div className='modal__content__body__addBlock'>
                         <span className="modal__content__body__addBlock__wrapper">
-                            <img src={add_img} alt=""/>
+                            {selectedImage ? (
+                                    <img 
+                                      src={selectedImage} 
+                                      alt="Uploaded"
+                                      className="modal__content__body__addBlock__wrapper__image"
+                                    />
+                                  ) : 
+                                  ( <>
+<img src={add_img} alt=""/>
                             <p className="modal__content__body__addBlock__wrapper__p">
                             Перетащите файл сюда или
                             </p>
@@ -27,10 +52,13 @@ export default function AddImageModal ({active,setActive}) {
                             name="uploadImg"
                             id='uploadImg'
                             accept="image/png, image/jpeg"
-                            // onChange={}
+                            onChange={handleFileUpload}
                             className='modal__content__body__addBlock__wrapper__outlineBtn' 
                             placeholder="Выберите файл">
                             </input>
+                            </> )
+                            }
+                            
                         </span>
                     </div>
 
