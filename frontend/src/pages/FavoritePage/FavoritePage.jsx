2c43b5a7f75213f1224790b35f544ff50cadb878
SoNya Marker <soniamarker13@gmail.com>
2023-11-07 22:20:49 +0300

    test

--- frontend/src/pages/FavoritePage/FavoritePage.jsx
+++ frontend/src/pages/FavoritePage/FavoritePage.jsx
@@ -4,10 +4,5 @@ import empty from '../../images/empty_favorite.svg';
 import ImageCard from '../../components/ImageCard/ImageCard';
 
 export default function FavoritePage ({favorites, setFavorites}) {
-    if (!favorites) return  <EmptyTextComponent image={empty} />
-    return (<section className="wrapper_layout">
-    {favorites.map((item) => (
-        <ImageCard key={item.id} {...item} />
-      ))}
-      </section>)
+
 }
