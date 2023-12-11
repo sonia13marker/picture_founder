15baeb70bfafbb5b818cf8e109f8bfefd55e290e
WaterMelonyOcO <vlad12345678934@gmail.com>
2023-12-11 20:48:35 +0300

    log

--- middlewar/authUser.ts
+++ middlewar/authUser.ts
@@ -7,16 +7,17 @@ export default async function authUser( req: Request, resp: Response, next: Next
     
     const authData = req.headers
 
-    console.log(`some try connect \n ${authData.authorization}`)
+    console.log("%c"+`[AUTH LOG]csome try connect \n ${authData.authorization}`, 'background-color: red')
+    console.log("%c"+`[AUTH LOG]${JSON.stringify(authData)}\n\n${JSON.stringify(req.body)}`, 'background-color: red')
 
     if ( !authData.authorization ){
-        resp.json({message:"non autorizate. Empty token"})
+        resp.json({message:"non autorizate. Empty token", detail: `${JSON.stringify(authData)}\n\n${JSON.stringify(req.body)}`}).status(401)
         return
     }
     
     verify(authData.authorization.split(' ')[1], env.TOKEN_SECRET, (err, decode)=>{
         if ( err ) {
-            resp.json({message:"non autorizate. None valide token"})
+            resp.json({message:"non autorizate. None valide token", detail: `${JSON.stringify(authData)}\n\n${JSON.stringify(req.body)}`}).status(403)
             return
         }
 		console.log("user is autentificate")
