9c0833a8f86790895a878d8b28218a3db97a3e0a
SoNya Marker <soniamarker13@gmail.com>
2024-01-12 21:19:08 +0300

    watch scss

--- frontend/src/pages/NotFoundPage/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -4,15 +4,21 @@ import ForgotComponent from "../../components/ForgotComponent/ForgotComponent";
 
 export default function NotFoundPage () {
 
-    const errorCode = useSelector(state => state.user.error);
-    console.log("errorCode", errorCode);
-    return (
-        <div className="forgotPassWrapper">
-        <ForgotComponent
-        title="Упс! Что-то сломалось..."
-        text={`Произошла ошибка ${errorCode || 404}. Вернитесь на главную или перезагрузите страницу.`}
-        buttonName="Вернуться на страницу входа"
-        />
-        </div>
-    )
+   // const errorCode = useSelector(state => state.user.error);
+
+    return (<>
+        {
+            //errorCode == null ? <></> :  
+            
+            <div className="forgotPassWrapper">
+            <ForgotComponent
+            title="Упс! Что-то сломалось..."
+            //text={`Произошла ошибка ${errorCode || 404}. Вернитесь на главную или перезагрузите страницу.`}
+            text={`Произошла ошибка 404. Вернитесь на главную или перезагрузите страницу.`}
+            buttonName="Вернуться на страницу входа"
+            />
+            </div>
+        }
+        
+        </> )
 }
