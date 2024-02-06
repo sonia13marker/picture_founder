662c027ff9141ce57a15ed0bf1dd8006f72d334f
WaterMelonyOcO <vlad12345678934@gmail.com>
2024-02-06 18:32:16 +0300

    test fix

--- api/image/dto/DataValidateDto.ts
+++ api/image/dto/DataValidateDto.ts
@@ -17,6 +17,6 @@ export const ImageScheme = Joi.object({
 
 export const ImageSchemeEdit = Joi.object({
     imageName: Joi.string().allow("", null),
-    imageTags: Joi.alternatives().try(Joi.array().items(Joi.string()), Joi.string()).allow("", null),
+    imageTags: Joi.alternatives().try(Joi.array().items(Joi.string()), Joi.string()).allow("", null, ''),
     isFavorite: Joi.boolean().allow("", null)
 })
