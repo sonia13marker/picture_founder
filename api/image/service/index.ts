d017c2e6708393ae5435641cdfd17083caaea34a
WaterMelonyOcO <vlad12345678934@gmail.com>
2024-02-09 00:10:30 +0300

    fix download image name

--- api/image/service/index.ts
+++ api/image/service/index.ts
@@ -238,7 +238,7 @@ export async function ImageDownload(imageId: string): Promise<string> {
     MyLogService("send image file")
 
     const fromSendImage = join(pathResolve.UserImageSaveDir(String(image?.ownerId)), String(image?.imageHash))
-    const toSendImage = join(pathResolve.UserImageUploadDir(), String(image?.imageName) + image?.ext)
+    const toSendImage = join(pathResolve.UserImageUploadDir(), String(image?.imageName) + "." + image?.ext)
 
     await copyFile(fromSendImage, toSendImage)
     // await rm(toSendImage)
