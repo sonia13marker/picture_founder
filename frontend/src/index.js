dbc828bc9ae7dee8b59bc998d840370ea363127a
SoNya Marker <soniamarker13@gmail.com>
2024-02-09 22:20:53 +0300

    hide logs in production

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,22 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/index';
-
 import { CookiesProvider } from 'react-cookie';
+
+//убирает логи в prod сборке
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => {}
+  console.error = () => {}
+  console.debug = () => {}
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   // <React.StrictMode>
   <CookiesProvider defaultSetOptions={{ path: '/' }}>
       <Provider store={store}>
-        <App />
+        <App ></App>
       </Provider>
       </CookiesProvider>
    /* </React.StrictMode> */
